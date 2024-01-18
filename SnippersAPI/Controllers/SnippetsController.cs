68aa4c6612f8d0ae0586eca8a2db96d23ba98358
Andrew <andrewozo123@gmail.com>
2024-01-18 16:43:48 -0500

    connected database and added snippet controller

---
+++ SnippersAPI/Controllers/SnippetsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SnippersAPI.Services;
+
+namespace SnippersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SnippetsController : ControllerBase
+    {
+        private readonly ISnippetService _snippetService;
+        public SnippetsController(ISnippetService snippetService)
+        {
+            _snippetService = snippetService;
+        }
+
+        [HttpGet("Snippets")]
+        public async Task<ActionResult<ServiceResponse<GetSnippetDto>>> GetAllSnippets()
+        {
+            return Ok(await _snippetService.GetAllSnippets());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ServiceResponse<GetSnippetDto>>> GetSingleSnippet(int id)
+        {
+            return Ok(await _snippetService.GetSnippetById(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetSnippetDto>>>> AddSnippet(AddSnippetDto newSnippet)
+        {
+            return Ok(await _snippetService.AddSnippet(newSnippet));
+        }
+    }
+}
