ab2981a42a249612387e14e75b397c8048fc0571
Andrew <andrewozo123@gmail.com>
2024-01-25 17:41:48 -0500

    Added EncryptionHelper and encrypted snippets body

---
+++ SnippersAPI/EncryptionHelper.cs
@@ -0,0 +1,60 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SnippersAPI
+{
+    public class EncryptionHelper
+    {
+
+        private static readonly string Key = "1234567890abcdef"; 
+        private static readonly string IV = "yourIV1234567890";
+
+        public static string Encrypt(string plainText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(Key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(IV);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string Decrypt(string cipherText)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Encoding.UTF8.GetBytes(Key);
+                aesAlg.IV = Encoding.UTF8.GetBytes(IV);
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- SnippersAPI/Services/SnippetService.cs
+++ SnippersAPI/Services/SnippetService.cs
@@ -18,7 +18,24 @@ namespace SnippersAPI.Services
         {
             var serviceResponse = new ServiceResponse<List<GetSnippetDto>>();
             var dbSnippets = await _context.Snippets.ToListAsync();
-            serviceResponse.Data = dbSnippets.Select(snip => _mapper.Map<GetSnippetDto>(snip)).ToList();
+
+            serviceResponse.Data = dbSnippets.Select(snip =>
+            {
+                var getSnippet = _mapper.Map<GetSnippetDto>(snip);
+
+                if (!string.IsNullOrEmpty(snip.Code))
+                {
+                    getSnippet.Code = EncryptionHelper.Decrypt(snip.Code);
+                }
+
+                if (!string.IsNullOrEmpty(snip.Language))
+                {
+                    getSnippet.Language = EncryptionHelper.Decrypt(snip.Language);
+                }
+
+                return getSnippet;
+            }).ToList();
+
             return serviceResponse;
         }
 
@@ -36,7 +53,12 @@ namespace SnippersAPI.Services
         public async Task<ServiceResponse<List<GetSnippetDto>>> AddSnippet(AddSnippetDto newSnippet)
         {
             var serviceResponse = new ServiceResponse<List<GetSnippetDto>>();
+
+            string encryptedCode = EncryptionHelper.Encrypt(newSnippet.Code);
+            string encryptedLanguage = EncryptionHelper.Encrypt(newSnippet.Language);
             var snippet = _mapper.Map<Snippet>(newSnippet);
+            snippet.Code = encryptedCode;
+            snippet.Language = encryptedLanguage;
             _context.Snippets.Add(snippet);
             await _context.SaveChangesAsync();
             serviceResponse.Data =
